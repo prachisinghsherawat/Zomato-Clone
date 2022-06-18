9c3c3858cf8ac61a4e0a1ace94feb2d5e90528cc
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-18 22:57:42 +0530

    Navbar

--- src/Components/AllRoutes/AllRoutes.jsx
+++ src/Components/AllRoutes/AllRoutes.jsx
@@ -1,9 +1,17 @@
+import { Routes , Route} from "react-router"
+import { Home } from "../Home/Home"
+
+
 
 
 export const AllRoutes = () => {
 
     return(
-        
-        <></>
+
+        <>
+        <Routes>
+            <Route path="/" element ={ <Home /> } />
+        </Routes>
+        </>
     )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,71 @@
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import { styled, alpha } from '@mui/material/styles';
 
+const Search = styled('div')(({ theme }) => ({
+
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
 
 export const Home = () => {
 
     return(
 
         <>
-        <div className="zomatoDiv"></div>
+        <div className="zomatoDiv">
+            <div className="zomatoLogo">
+                <img src="https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png" alt="" height="100%" width="100%"/>
+            </div>
+
+            <p>Discover the best Food and Drinks at you place</p>
+
+            <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+            </Search>
+            
+        </div>
         </>
     )
 }
