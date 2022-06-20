eefb1f51b2dc7ace2beab8d05ff2f60949c51eda
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-20 22:46:05 +0530

    Zomato Navbar

--- src/Components/Navbar/ZomatoNav.jsx
+++ src/Components/Navbar/ZomatoNav.jsx
@@ -72,73 +72,7 @@ export const ZomatoNav = () => {
 
         </div> 
 
-             
-        <div className="tabsDiv">
-
-            <Box sx={{ width: '100%' }}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <Tabs value={value} onChange={HandleChange} aria-label="basic tabs example">
-                        <Tab label="Delivery" {...a11yProps(0)} />
-                        <Tab label="Dinning Out" {...a11yProps(1)} />
-                        <Tab label="Nightlife" {...a11yProps(2)} />
-                    </Tabs>
-                </Box>
-
-              <TabPanel value={value} index={0}>
-                {/* Item One */}
-              </TabPanel>
-              <TabPanel value={value} index={1}>
-                {/* Item Two */}
-              </TabPanel>
-              <TabPanel value={value} index={2}>
-                {/* Item Three */}
-              </TabPanel>
-
-
-            </Box>
-
-        </div>
-
-
-
-
-        
         </>
     )
 }
-
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
 
