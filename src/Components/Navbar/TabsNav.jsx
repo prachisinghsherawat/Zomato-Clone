ee7b8b1aa00a9e40aaa391ed2f71e02ae26da3ef
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-20 23:48:07 +0530

    Tabs Navbar

--- src/Components/Navbar/TabsNav.jsx
+++ src/Components/Navbar/TabsNav.jsx
@@ -1,4 +1,11 @@
 import "./Navbar.css"
+import delivery from "../Icons/delivery.png"
+import order from "../Icons/order.png"
+import night from "../Icons/night.png"
+import deliveryCol from "../Icons/deliveryCol.png"
+import orderCol from "../Icons/orderCol.png"
+import nightCol from "../Icons/nightCol.png"
+
 
 export const TabsNav = () => {
 
@@ -7,17 +14,27 @@ export const TabsNav = () => {
         <>
             <div className="tabs">
                 <div>
-                    <img src="" alt="" height="100%" width="100%"/>
+                    <div onClick={} className="icontab">
+                        <img src={delivery} alt="" height="100%" width="100%"/>
+                    </div>
                     <p>Delivery</p>
                 </div>
+
                 <div>
-                    <img src="" alt="" height="100%" width="100%"/>
-                    <p>Dinning Out</p>
-                </div>
-                <div>
-                    <img src="" alt="" height="100%" width="100%"/>
+                    <div onClick={} className="icontab">
+                        <img src={night} alt="" height="100%" width="100%"/>
+                    </div>
                     <p>Nightlife</p>
                 </div>
+
+                <div>
+                    <div onClick={} className="icontab">
+                        <img src={order} alt="" height="100%" width="100%"/>
+                    </div>
+                    <p>Dinning Out</p>
+                </div>
+
+                
             </div>
         </>
     )
