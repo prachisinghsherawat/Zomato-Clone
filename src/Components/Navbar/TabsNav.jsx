0b803ea35bfe2237ea2b81c9192a1b5da74a9cef
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-20 23:13:57 +0530

    Icons

--- src/Components/Navbar/TabsNav.jsx
+++ src/Components/Navbar/TabsNav.jsx
@@ -1,12 +1,24 @@
-
-
+import "./Navbar.css"
 
 export const TabsNav = () => {
 
     return(
 
         <>
-        <div className="tabs"></div>
+            <div className="tabs">
+                <div>
+                    <img src="" alt="" height="100%" width="100%"/>
+                    <p>Delivery</p>
+                </div>
+                <div>
+                    <img src="" alt="" height="100%" width="100%"/>
+                    <p>Dinning Out</p>
+                </div>
+                <div>
+                    <img src="" alt="" height="100%" width="100%"/>
+                    <p>Nightlife</p>
+                </div>
+            </div>
         </>
     )
 }
