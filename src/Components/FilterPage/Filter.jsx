0798069b3abf51febb8efd3c75c78d0e20ffdc94
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-21 19:59:58 +0530

    Filter Food

--- src/Components/FilterPage/Filter.jsx
+++ src/Components/FilterPage/Filter.jsx
@@ -1,12 +1,59 @@
 
 import "./Filter.css"
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-export const Filter = () => {
+export function Filter() {
+  const [age, setAge] = React.useState('');
 
-    return(
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
-        <>
-        <div className="filterPart"></div>
-        </>
-    )
+  return (
+
+      <div className="filterPart">
+
+           <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <InputLabel id="demo-select-small">Price</InputLabel>
+                <Select
+                    labelId="demo-select-small"
+                    id="demo-select-small"
+                    value={age}
+                    label="Price"
+                    onChange={handleChange}>
+
+                   <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <InputLabel id="demo-select-small">Rating</InputLabel>
+                <Select
+                    labelId="demo-select-small"
+                    id="demo-select-small"
+                    value={age}
+                    label="Rating"
+                    onChange={handleChange}>
+
+                   <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+
+      </div>
+  );
 }
+
---
+++ src/Components/FilterPage/FoodFilter.jsx
@@ -0,0 +1,12 @@
+
+
+export const FoodFilter = () => {
+
+    return(
+
+        <>
+
+        <div className="foodFilter"></div>
+        </>
+    )
+}
