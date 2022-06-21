7c5c0e6da1aa8aa7a6277de387f861be51f55fd9
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-22 00:01:53 +0530

    Food Items

--- src/Components/Food/FoodItems.jsx
+++ src/Components/Food/FoodItems.jsx
@@ -3,11 +3,18 @@ import { ZomatoNav } from "../Navbar/ZomatoNav"
 import { Filter } from "../FilterPage/Filter"
 import { FoodFilter } from "../FilterPage/FoodFilter"
 import {FoodData}  from "../Data/FilterData"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 
 export const FoodItems = () => {
 
-    console.log(FoodData)
+    const [randomData , setRandomData] = useState([])
+    useEffect(()=>{GetRandomData()},[])
+
+    const GetRandomData = () => {
+        axios.get("http://localhost:8080/random").then((res)=>setRandomData(res.data))
+    }
 
     return(
         
@@ -18,7 +25,14 @@ export const FoodItems = () => {
         < Filter />
         < FoodFilter data = {FoodData}/>
 
-        
+        {/* <div className="random">
+            {randomData.map((el)=>(
+
+                <div>
+                    <img src={el.imgUrl} />
+                </div>
+            ))}
+        </div> */}
 
         </>
     )
