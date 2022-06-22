ba3c141f7090d28fb81f7192c6157f1720f21be0
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-22 23:01:56 +0530

    All Routes

--- src/Components/AllRoutes/AllRoutes.jsx
+++ src/Components/AllRoutes/AllRoutes.jsx
@@ -1,8 +1,13 @@
+
 import { Routes , Route} from "react-router"
+import { Cake } from "../Food/Cake"
+import { Chaat } from "../Food/Chaat"
+import { Coffee } from "../Food/Coffee"
 import { Random } from "../Food/Random"
 import { Home } from "../Home/Home"
-
-
+import { IceCream } from "../Food/IceCream"
+import { Pizza } from "../Food/Pizza"
+import { Shake } from "../Food/Shake"
 
 
 export const AllRoutes = () => {
@@ -15,12 +20,12 @@ export const AllRoutes = () => {
             <Route path="/" element ={ <Home /> } />
             <Route path="/food" element ={ <Random /> } />
             <Route path="/food/burger" element ={ <Burger /> } />
-            <Route path="/food/cake" element ={ <Random /> } />
-            <Route path="/food/coffee" element ={ <Random /> } />
-            <Route path="/food/chaat" element ={ <Random /> } />
-            <Route path="/food/ice-cream" element ={ <Random /> } />
-            <Route path="/food/pizza" element ={ <Random /> } />
-            <Route path="/food/shake" element ={ <Random /> } />
+            <Route path="/food/cake" element ={ <Cake /> } />
+            <Route path="/food/coffee" element ={ <Coffee /> } />
+            <Route path="/food/chaat" element ={ <Chaat /> } />
+            <Route path="/food/ice-cream" element ={ <IceCream /> } />
+            <Route path="/food/pizza" element ={ <Pizza /> } />
+            <Route path="/food/shake" element ={ <Shake /> } />
             
         </Routes>
         </>
