f790d09f19ed734ee0828ac7f0701681585720c3
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-22 23:37:10 +0530

    Pizza Data

--- src/Components/Food/Pizza.jsx
+++ src/Components/Food/Pizza.jsx
@@ -1,8 +1,48 @@
-
+import { TabsNav } from "../Navbar/TabsNav"
+import { ZomatoNav } from "../Navbar/ZomatoNav"
+import { Filter } from "../FilterPage/Filter"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import "./A.Food.css"
 
 export const Pizza = () => {
 
+    const [PizzaData , setPizzaData] = useState([])
+    useEffect(()=>{GetRandomData()},[])
+
+    const GetRandomData = () => {
+        axios.get("http://localhost:8080/Pizza").then((res)=> setPizzaData(res.data))
+    }
+
     return(
-        <></>
+
+        <>
+
+        < ZomatoNav />
+        < TabsNav />
+        < Filter />
+
+        <h1 id="headOrder"> Order your Pizza </h1>
+
+        <div className="random">
+
+            {PizzaData.map((el)=>(
+
+                <div >
+
+                    <div className="imgDiv"><img src={el.imgUrl} /></div>
+                    <div className="flxBox">
+                        <h1>{el.name}</h1>
+                        <span>{el.rating}</span>
+                    </div>
+                    <p>{el.variety}</p>
+                    
+                    
+
+                </div>
+            ))}
+        </div>
+
+        </>
     )
 }
