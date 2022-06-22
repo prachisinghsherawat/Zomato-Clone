b9de22512a692e29ae16697183d40e10f5ae862f
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-22 23:34:17 +0530

    Ice-Cream Data

--- src/Components/Food/IceCream.jsx
+++ src/Components/Food/IceCream.jsx
@@ -1,8 +1,48 @@
-
+import { TabsNav } from "../Navbar/TabsNav"
+import { ZomatoNav } from "../Navbar/ZomatoNav"
+import { Filter } from "../FilterPage/Filter"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import "./A.Food.css"
 
 export const IceCream = () => {
 
+    const [iceCreamData , setIceCreamData] = useState([])
+    useEffect(()=>{GetRandomData()},[])
+
+    const GetRandomData = () => {
+        axios.get("http://localhost:8080/IceCream").then((res)=> setIceCreamData(res.data))
+    }
+
     return(
-        <></>
+
+        <>
+
+        < ZomatoNav />
+        < TabsNav />
+        < Filter />
+
+        <h1 id="headOrder"> Order your IceCream </h1>
+
+        <div className="random">
+
+            {iceCreamData.map((el)=>(
+
+                <div >
+
+                    <div className="imgDiv"><img src={el.imgUrl} /></div>
+                    <div className="flxBox">
+                        <h1>{el.name}</h1>
+                        <span>{el.rating}</span>
+                    </div>
+                    <p>{el.variety}</p>
+                    
+                    
+
+                </div>
+            ))}
+        </div>
+
+        </>
     )
 }
