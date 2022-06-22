c05d848928b1a4959e84999fe0ece94543e36590
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-22 23:28:17 +0530

    Cake Data

--- src/Components/Food/Cake.jsx
+++ src/Components/Food/Cake.jsx
@@ -1,8 +1,48 @@
-
+import { TabsNav } from "../Navbar/TabsNav"
+import { ZomatoNav } from "../Navbar/ZomatoNav"
+import { Filter } from "../FilterPage/Filter"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import "./A.Food.css"
 
 export const Cake = () => {
 
+    const [cakeData , setCakeData] = useState([])
+    useEffect(()=>{GetRandomData()},[])
+
+    const GetRandomData = () => {
+        axios.get("http://localhost:8080/Cake").then((res)=> setCakeData(res.data))
+    }
+
     return(
-        <></>
+
+        <>
+
+        < ZomatoNav />
+        < TabsNav />
+        < Filter />
+
+        <h1 id="headOrder"> Order your cake </h1>
+
+        <div className="random">
+
+            {cakeData.map((el)=>(
+
+                <div >
+
+                    <div className="imgDiv"><img src={el.imgUrl} /></div>
+                    <div className="flxBox">
+                        <h1>{el.name}</h1>
+                        <span>{el.rating}</span>
+                    </div>
+                    <p>{el.variety}</p>
+                    
+                    
+
+                </div>
+            ))}
+        </div>
+
+        </>
     )
 }
