e6f30e4989713421ba0f6a3cb057ced24f2c382e
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-23 18:52:50 +0530

    Landing Page

--- src/Components/Home/Landing.jsx
+++ src/Components/Home/Landing.jsx
@@ -11,20 +11,29 @@ export const LandingPage = () => {
         <div className="cardBox">
 
             <div onClick={()=> navigate("/delivery")}>
+
                 <img src="https://b.zmtcdn.com/data/pictures/3/20066713/8d55259349a583f56d540b716c249f1a_o2_featured_v2.jpg" alt="" height="100%" width="100%" />
                 <h1>Order Online</h1>
                 <p>Stay home and Order to your doorsteps</p>
+
             </div>
-            
-            <div>
+
+
+            <div onClick={()=> navigate("/dinning-out")}>
+
                 <img src="https://b.zmtcdn.com/data/pictures/4/307374/b3309d3aad3838b482586ca304a7dcc0_featured_v2.jpg?output-format=webp" alt="" height="100%" width="100%" />
                 <h1>Dinning Out</h1>
                 <p>View the city's favourite dining venues</p>
+
             </div>
-            <div>
+
+
+            <div onClick={()=> navigate("/nightlife")}>
+
                 <img src="https://b.zmtcdn.com/data/pictures/7/19291447/5243902b4cb774b2cc36556e40a6170d_featured_v2.jpg" alt="" height="100%" width="100%" />
                 <h1>Nightlife and Clubs</h1>
                 <p>Explore the city's top nightlife outlets </p>
+
             </div>
         </div>
         </>
