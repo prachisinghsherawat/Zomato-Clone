4ca29511010d7ceb64cee68831c96344cd807456
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-27 16:05:42 +0530

    Search Details

--- src/Components/DetailsPage/SearchDetails.jsx
+++ src/Components/DetailsPage/SearchDetails.jsx
@@ -1,26 +1,50 @@
 import { useParams } from "react-router"
 import {useState , useEffect} from "react"
 import axios from "axios"
+import { ZomatoNav } from "../Navbar/ZomatoNav"
+import "./Details.css"
 
 
 export const SearchDetails = () => {
 
     const {id} = useParams()
-    const [searchData , setSearchData] = useState([])
+    const [searchData , setSearchData] = useState({})
     useEffect(() => {GetSearchData()},[])
 
 
     const GetSearchData = () => {
         axios.get(`http://localhost:8080/global/${id}`).then((res)=> setSearchData(res.data))
     }
-    console.log(searchData)
+    //console.log(searchData)
 
     return(
 
         <>
-        {searchData.map((el)=>(
-            <div></div>
-        ))}
+            <ZomatoNav />
+
+            <div className="FoodDetails">
+
+                <div><img src={searchData.imgUrl} /></div>
+
+                <div id="FlexBoxis">
+                    <h1>{searchData.name}</h1>
+                    <span> Rs . {searchData.price} /-</span>
+                </div>
+
+                <p>{searchData.variety}</p>
+
+                <div id="keyPair">
+                    <span>Location : </span>
+                    <p>{searchData.place}</p>
+                </div>
+
+                <div id="keyPair">
+                    <span>Rating : </span> 
+                    <p>{searchData.rating}</p>
+                </div>
+
+            </div>
+
         </>
     )
 }
