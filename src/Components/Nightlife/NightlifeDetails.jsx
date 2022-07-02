9d89a6893d14184ba83409bce45905a08ac04d6d
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-07-02 23:47:27 +0530

    Nightlife

---
+++ src/Components/Nightlife/NightlifeDetails.jsx
@@ -0,0 +1,57 @@
+import { useParams } from "react-router"
+import {useState , useEffect} from "react"
+import axios from "axios"
+import { ZomatoNav } from "../Navbar/ZomatoNav"
+// import "./A.Details.css"
+import { Footer } from "../Footer/Footer"
+
+
+export const NightDetails = () => {
+
+    const {id} = useParams()
+    const [NightData , setNightData] = useState({})
+    useEffect(() => {GetNightData()},[])
+
+    useEffect(()=>{window.scrollTo({ top: 0, behavior: "smooth" })},[])
+
+
+    const GetNightData = () => {
+        axios.get(`http://localhost:8080/restaurants/${id}`).then((res)=> setNightData(res.data))
+    }
+    //console.log(NightData)
+
+    return(
+
+        <>
+            <ZomatoNav />
+
+            <div className="FoodDetails">
+
+                <div><img src={NightData.imgUrl} /></div>
+
+                <div id="FlexBoxis">
+                    <h1>{NightData.name}</h1>
+                    <span> Rs . {NightData.price}</span>
+                </div>
+
+                <p>{NightData.variety}</p>
+
+                <div id="keyPair">
+                    <span>Location : </span>
+                    <p>{NightData.place}</p>
+                </div>
+
+                <div id="keyPair">
+                    <span>Rating : </span> 
+                    <p>{NightData.rating}</p>
+                </div>
+
+            </div>
+
+            <div className="footerDiv">
+                <Footer />
+            </div>
+
+        </>
+    )
+}
