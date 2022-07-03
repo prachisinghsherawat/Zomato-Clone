5e9b5b78245e8efaeb9f84b7361507563018bb41
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-07-04 00:09:46 +0530

    Sign Up

--- src/Components/Authentication/Signup.jsx
+++ src/Components/Authentication/Signup.jsx
@@ -1,8 +1,50 @@
-
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import "./Authentication.css"
 
 export const SignUp = () => {
 
     return(
-        <></>
+
+        <>
+
+        <div className="signup">
+            
+            <div className="up">
+                <p>Sign up</p>
+                <p>x</p>
+            </div>
+        
+        
+            <Box
+               sx={{
+                width: 500,
+                maxWidth: '100%',
+               }}>
+               <TextField fullWidth label="Full Name" id="full_name" />
+            </Box><br />
+
+            <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}>
+              <TextField fullWidth label="Email" id="email" />
+            </Box><br />
+
+            <button id='submit'>Create account</button>
+
+            <div className='or'>
+                <p>or</p>
+            </div>
+
+            <button>Continue with Google</button>
+
+        </div>
+
+
+
+        </>
     )
 }
