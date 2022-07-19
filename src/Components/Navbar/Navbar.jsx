6bf588f1afd1e9190c83b9b731000b404f5bc83d
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-07-19 14:15:51 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -131,7 +131,7 @@ export const Navbar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src={photoURL? photoURL : ""} />
+                <Avatar alt="Remy Sharp" src={photoURL} />
               </IconButton>
             </Tooltip>
             <Menu
