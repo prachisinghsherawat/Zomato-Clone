b8e602f199f36c173e339759ea44b84bf994a564
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-08-16 20:13:52 +0530

    Cart Page

--- src/Components/CartPage/CartPage.jsx
+++ src/Components/CartPage/CartPage.jsx
@@ -1,9 +1,21 @@
-
+import { useEffect } from "react"
 
 
 export const CartPage = ({foodData}) => {
 
-    console.log(foodData)
+    const [cartData , setCartData] = useState([])
+    useEffect(()=> {addFoodData()},[])
+
+    const addFoodData = () => {
+
+        let data = {...foodData ,quantity : 1}
+        axios.post("https://zomatodataapi.herokuapp.com/cart",data).then(()=> getCartData())
+    }
+
+    const getCartData = () => {
+        axios.get("https://zomatodataapi.herokuapp.com/cart").then((res)=> setCartData(res.data))
+    }
+
 
     return(
         
