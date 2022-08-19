fbf66423b2e0aa5d7f9b4e5b226dc158049566a6
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-08-19 09:49:00 +0530

    Payment Successful Page

---
+++ src/Components/PaymentPage/PaymentSuccessful.jsx
@@ -0,0 +1,11 @@
+
+
+
+export const PaymentSuccessful = () => {
+
+    return(
+        <div className="paySuccess">
+            <img src="http://www.shikharclasses.in/wp-content/uploads/2020/04/PAYMENT-SUCCESS.png" alt="" height="100%" width="100%" />
+        </div>
+    )
+}
